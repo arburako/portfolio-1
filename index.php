d04751f4b0d3c875c1cb5a06f88ae9039bc7087d
Jessica Crabb <jessica.ann.crabb@gmail.com>
2012-10-25 20:56:53 +0200

    Modified home styling

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 	<link rel="stylesheet" href="css/main.css" />
 	<link rel="stylesheet" href="css/sofia.css" type="text/css" charset="utf-8" />
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css' />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
 	<script src="js/main.js"></script>
 </head>
 <body>
@@ -22,7 +22,7 @@
           <a href="#portfolio"><img src="img/logo.png" ></a>
         </div>
         <div id="nav">
-          <div class="box">
+          <div class="box last">
             <ul>
               <li><a href="#resume">Resume</a></li>
               <li><a href="#contact">Contact</a></li>
@@ -36,15 +36,15 @@
           </div>	
         </div>	
         <div id="name">
-          <h1>JESSICA CRABB</h1>
-          <h2>UX | IxD | UI</h2>
+          <h1>JESSICA</h1>
+          <h2>CRABB</h2>
         </div>
       </div>
     </div>
   </div>
   <div class="top">
     <div id="featured">
-      <img id="featured-img" src="img/featured.jpg" />
+      <img id="featured-img" src="img/featured.png" />
       <div id="featured-text">
 		  <span id="tags">FEATURED PROJECT | UI DESIGN, UX, CONCEPT</span>
 		  <span id="name">TASK PLANET ANDROID APP</span>
@@ -52,7 +52,25 @@
 		 <a href="#project1">VIEW PROJECT</a>
   	  </div>
     </div>
- 
   </div>
+  
+  <div class="content">
+  	<span id="content-header">ALL PORTFOLIO PROJECTS</span>
+  	
+  	<div class="portfolio-project">
+  		<a href="#project2"><img src="img/content.jpg "/></a>
+  	</div>
+  	
+  	<div class="portfolio-project">
+  		<a href="#project3"><img src="img/content.jpg "/></a>
+  	</div>
+  	
+  	<div class="portfolio-project last">
+  		<a href="#project4"><img src="img/content.jpg "/></a>
+  	</div>
+  	
+  	
+  </div>
+  
 </body>
 </html>
