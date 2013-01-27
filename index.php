f99ab4a909c8972b09ac43e67686ab781c498641
Jessica Crabb <jessica.ann.crabb@gmail.com>
2013-01-27 03:24:04 -0500

    love chris

--- index.php
+++ index.php
@@ -11,65 +11,91 @@
 
 	<link rel="stylesheet" href="css/main.css" />
 	<link rel="stylesheet" href="css/sofia.css" type="text/css" charset="utf-8" />
+  <link rel="stylesheet" type="text/css" href="css/MyFontsWebfontsKit.css">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
 	<script src="js/main.js"></script>
 </head>
 <body>
-  <div id="header-offset" class="top">
-    <div id="header-container">
-      <div id="header">
-        <div id="logo">
-          <a href="#portfolio"><img src="img/logo.png" ></a>
-        </div>
-        <div id="nav">
-          <div class="box">
-            <ul>
-              <li><a href="#resume">Resume</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          </div>
-          <div class="box">
-            <ul>
-              <li><a class="current-page" href="#portfolio">Portfolio</a></li>
-              <li><a href="#about">About</a></li>
-            </ul>
-          </div>	
-        </div>	
-        <div id="name">
-          <h1>JESSICA CRABB</h1>
-          <h2>UX | IxD | UI</h2>
-        </div>
+  <div id="header">
+    <div id="logo">
+      <a href="#portfolio"><img src="img/logo.png" ></a>
+    </div>
+    <div id="nav">
+      <div class="box">
+        <ul>
+          <li><a href="#resume">RESUME</a></li>
+          <li><a href="#contact">CONTACT</a></li>
+        </ul>
       </div>
+      <div class="box">
+        <ul>
+          <li><a class="current-page" href="#portfolio">PORTFOLIO</a></li>
+          <li><a href="#about">ABOUT</a></li>
+        </ul>
+      </div>	
+    </div>	
+    <div id="name">
+      <h1>JESSICA CRABB</h1>
+      <h2>User Experience Design</h2>
     </div>
   </div>
+
   <div class="top">
     <div id="featured">
-      <img id="featured-img" src="img/featured.png" />
+      <a id="feat" href="#port1"><img id="featured-img" src="img/featured_tp_green.png" /></a>
       <div id="featured-text">
-		  <span id="tags">FEATURED PROJECT | UI DESIGN, UX, CONCEPT</span>
-		  <span id="name">TASK PLANET ANDROID APP</span>
+		  <span id="tags">FEATURED PROJECT</span>
+		  <span id="name">TASK PLANET</span>
+      <span id="name2">ANDROID APP</span>
 		  <span id="desc">Complete your to-do list to save the world!</span>
-		 <a href="#project1">VIEW PROJECT</a>
-  	  </div>
+		  <a id ="oldhov" href="#project1">UI | UX</a>
     </div>
   </div>
   
   <div class="content">
+    <div id="content-header-box">
   	<span id="content-header">ALL PORTFOLIO PROJECTS</span>
-  	
+  	</div>
   	<div class="portfolio-project">
-  		<a href="#project2"><img src="img/content.jpg "/></a>
+		<div class="img">
+			<div>
+			</div>
+			<img src="img/cbthumb_green.png "/>
+		</div>
   	</div>
   	
   	<div class="portfolio-project">
-  		<a href="#project3"><img src="img/content.jpg "/></a>
+		<div class="img">
+			<div>
+			</div>
+			<img src="img/tpthumb_green.png "/>
+		</div>
   	</div>
   	
   	<div class="portfolio-project last">
-  		<a href="#project4"><img src="img/content.jpg "/></a>
+		<div class="img">
+			<div>
+			</div>
+			<img src="img/rfthumb_green.png "/>
+		</div>
   	</div>
   	
   	
+  </div>
+
+  <div class="footer">
+    <div id="footer_about">
+      <span id="footer_about_header">ABOUT</span>
+      <span id="footer_about_text">Jessica Crabb, Computational Media major </br> Georgia Institute of Technology</span>
+    </div>
+    <div id="footer_resume">
+      <span id="footer_resume_header">RESUME</span>
+      <span id="footer_resume_text">View resume in browser</br> Download as PDF</span>
+    </div>
+    <div id="footer_contact">
+      <span id="footer_contact_header">CONTACT</span>
+      <span id="footer_contact_text">jessica.crabb@gatech.edu</br> 912-337-1764</span>
+    </div>
   </div>
   
 </body>
---
+++ oldindex.html
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="shortcut icon" href="img/favicon.ico" />
+
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Jessica Crabb | UX Portfolio</title>
+	<meta name="description" content="" />
+
+
+	<link rel="stylesheet" href="css/main.css" />
+	<link rel="stylesheet" href="css/sofia.css" type="text/css" charset="utf-8" />
+  <link rel="stylesheet" type="text/css" href="css/MyFontsWebfontsKit.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+	<script src="js/main.js"></script>
+</head>
+<body>
+  <div id="header-offset">
+    <div id="header-container">
+      <div id="header">
+        <div id="logo">
+          <a href="#portfolio"><img src="img/logo.png" ></a>
+        </div>
+        <div id="nav">
+          <div class="box">
+            <ul>
+              <li><a href="#resume">RESUME</a></li>
+              <li><a href="#contact">CONTACT</a></li>
+            </ul>
+          </div>
+          <div class="box">
+            <ul>
+              <li><a class="current-page" href="#portfolio">PORTFOLIO</a></li>
+              <li><a href="#about">ABOUT</a></li>
+            </ul>
+          </div>	
+        </div>	
+        <div id="name">
+          <h1>JESSICA CRABB</h1>
+          <h2>UX | IxD | UI</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="top">
+    <div id="featured">
+      <img id="featured-img" src="img/featured.png" />
+      <div id="featured-text">
+		  <span id="tags">FEATURED PROJECT | UI DESIGN, UX, CONCEPT</span>
+		  <span id="name">TASK PLANET ANDROID APP</span>
+		  <span id="desc">Complete your to-do list to save the world!</span>
+		 <a href="#project1">VIEW PROJECT</a>
+  	  </div>
+    </div>
+  </div>
+  
+  <div class="content">
+  	<span id="content-header">ALL PORTFOLIO PROJECTS</span>
+  	
+  	<div class="portfolio-project">
+		<div class="img">
+			<div>
+			</div>
+			<img src="img/cbthumb.png "/>
+		</div>
+  	</div>
+  	
+  	<div class="portfolio-project">
+		<div class="img">
+			<div>
+			</div>
+			<img src="img/tpthumb.png "/>
+		</div>
+  	</div>
+  	
+  	<div class="portfolio-project last">
+		<div class="img">
+			<div>
+			</div>
+			<img src="img/rfthumb.png "/>
+		</div>
+  	</div>
+  	
+  	
+  </div>
+  
+</body>
+</html>
